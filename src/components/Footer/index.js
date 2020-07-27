91999f64ffa503e05a95c7a5b1df15af91a69ace
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-27 19:07:36 -0300

    setup inicial do projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img src="https://fontmeme.com/permalink/200727/37383bccae5d4acbf71e1f7625355ce6.png" alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
