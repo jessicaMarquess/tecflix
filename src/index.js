17a78b723885dd755fecfb7ddc8c9915ae632fde
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-29 11:42:27 -0300

    commit add routes and pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Pagina404 = () => (
+  <div>
+    Página 404
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component = {Home} exact/> 
+      <Route path="/cadastro/video" component = {CadastroVideo} />
+      <Route path="/cadastro/categoria" component = {CadastroCategoria} />
+      <Route component= {Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
